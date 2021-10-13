0e0b960d64d0505d207df1d61bffbb49584a1422
Miles Weber <vorvon4@gmail.com>
2021-10-13 11:48:47 -0400

    Initial Commit

---
+++ PoppinTextEditor.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class PoppinTextEditor{
+    public static void main(String[] args){
+        JFrame frame = new JFrame("PoppinTextEditor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);        
+    }
+}
